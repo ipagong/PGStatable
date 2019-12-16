fd61dd32e172ec8db30617b3aefdf7aea43ec7c1
ipagong <ipagong.dev@gmail.com>
2019-12-16 18:54:02 +0900

    StateSceneFactory 의 역할을 각 ViewController가 구현할수 있도록 StateSceneType 확장.

--- PGStatable/Classes/StateType.swift
+++ PGStatable/Classes/StateType.swift
@@ -1,35 +1,43 @@
 //
 //  StateType.swift
+//  
 //
 //  Created by ipagong on 09/07/2019.
 //  Copyright © 2019 suwan.park All rights reserved.
 //
 
-import Foundation
+import UIKit
 
+/// StateType의 abstract 인터페이스
 public protocol AnyStateType { }
 
-public protocol StateType : AnyStateType, Equatable {
-    var dataKey:String?             { get }
-    var identifier:String           { get }
-    var hasScene:Bool               { get }
+/// 실제 서비스에서 구현해아할 State(상태) 인터페이스
+public protocol StateType : AnyStateType {
+    associatedtype ElementKey : Equatable
+    
+    /// 상태별 Equtable을 데이타 비교를 위한 프로퍼티.
+    var dataKey:ElementKey? { get }
+    
+    /// 해당 상태가 Scene의 존재 여부를 위한 프로퍼티.
+    var hasScene:Bool { get }
 }
 
+extension StateType {
+    var hasScene:Bool { return true }
+}
+
+/// 상태별 Scene을 노출할 베이스갈 ViewController 지정하기 위한 인터페이스.
 public protocol StateSceneFactory {
-    var storyboardIdentifier : String? { get }
-    var vcIdentifier: String? { get }
+    /// Scene에서 주입 받을 데이타.
+    var sceneValue:Any? { get }
     
-    func bindData(with scene:StateSceneType?)
+    /// StateSceneType을 구현한 구현체의 타입을 반환하는 프로퍼티.
+    var sceneType:StateSceneType.Type { get }
 }
 
 public extension StateSceneFactory where Self : StateType {
+    /// scene의 팩토리 메소드. 구현체의 타입을 실제화 하여 인터페이스로 반환하는 컴퓨티드 프로퍼티.
     var scene:StateSceneType? {
-        guard let storyboard = self.storyboardIdentifier else { return nil }
-        guard let identifier = self.vcIdentifier         else { return nil }
-        
-        guard let vc = UIStoryboard(name: storyboard, bundle: nil).instantiateViewController(withIdentifier: identifier) as? StateSceneType else { return nil }
-        vc.stateIdentifier = self.identifier
-        self.bindData(with: vc)
-        return vc
+        return self.sceneType.createIntance(self)
     }
 }
